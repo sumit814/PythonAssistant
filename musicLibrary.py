c025e52f303f029fd7a338a98050e90807d19cfb
Sumit Chaturvedi <163818228+sumit814@users.noreply.github.com>
2025-05-11 03:01:16 +0530

    musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "BidaSada": "https://youtu.be/PbUj1PAhKnQ?si=iitWdWwJaLW7ciPk",
+    "happy": "https://youtu.be/55c6IlV7BEo?si=0ja61yhWSsM9Bd9c"
+}
